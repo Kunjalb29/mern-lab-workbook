aaadce3140d44609df5645b8d4c0f6ed4e25fb02
Microsoft Engineer <engineer@microsoft.com>
2026-04-22 00:35:44 +0530

    Implemented Material UI code for Experiment 4

---
+++ experiments/exp4-material-ui/code/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button, Typography, Container } from '@mui/material';
+
+function App() {
+  return (
+    <Container>
+      <Typography variant="h4" gutterBottom>Material UI Example</Typography>
+      <Button variant="contained" color="primary">Click Me</Button>
+    </Container>
+  );
+}
+export default App;
