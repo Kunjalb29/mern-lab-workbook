2884272028c4b0ce8e7075021eb008b92b03dcd6
Microsoft Engineer <engineer@microsoft.com>
2026-04-22 00:42:27 +0530

    Implemented React Components code for Experiment 1

---
+++ experiments/exp1-react-components/code/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Greeting = () => <h1>Hello, World!</h1>;
+
+function App() {
+  return (
+    <div>
+      <Greeting />
+    </div>
+  );
+}
+export default App;
