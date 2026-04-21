fe1c3b7584167843bc0219e2c18458863090ca0a
Microsoft Engineer <engineer@microsoft.com>
2026-04-22 00:35:49 +0530

    Implemented Bootstrap code for Experiment 5

---
+++ experiments/exp5-bootstrap/code/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-primary">Bootstrap in React</h1>
+      <button className="btn btn-success">Success Button</button>
+    </div>
+  );
+}
+export default App;
